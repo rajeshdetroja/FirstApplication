a7207c3fcc7bb74c9c4d3f2e67d5d4a2c6f4dd20
rajeshdetroja <rajudetroja@outlook.com>
2015-03-24 15:19:48 +0530

    f
    f

---
+++ GitHubApplication/Controllers/GitHubController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubApplication.Controllers
+{
+    public class GitHubController : Controller
+    {
+        //
+        // GET: /GitHub/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
